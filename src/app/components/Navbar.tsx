08e0a43877c4b3c12212afdb66dabef985b7a040
andres34cortez <andrescel34@gmail.com>
2024-02-09 14:38:29 -0300

    el mundo es hermoso

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,6 @@ const Navbar: React.FC = () => {
 
   const handleMenuToggle = () => {
     setMenuOpen(!menuOpen);
- 
   };
 
   return (
@@ -18,60 +17,79 @@ const Navbar: React.FC = () => {
         <div className='flex-shrink-0'>
           <Image src={logoNavbar} alt='Logo' width={120} height={40} />
         </div>
-        <div className='md:hidden'>
-          <div onClick={handleMenuToggle} className='text-white'>
-            texto grande
+        <div className='md:hidden ml-auto mr-4'>
+          <div onClick={handleMenuToggle} className='text-white cursor-pointer'>
+            ☰
           </div>
         </div>
-        <div
-          className={`${
-            menuOpen ? "flex" : "hidden"
-          } md:flex md:space-x-6 ml-auto`}
-        >
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+
+        <div className={" hidden  md:block md:space-x-6 ml-auto"}>
+          <a href='#' className='text-white text-xs  hover:text-gray-300'>
             LA EMPRESA
           </a>
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+          <a href='#' className='text-white  text-xs  hover:text-gray-300'>
             PRENSA
           </a>
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+          <a href='#' className='text-white text-xs  hover:text-gray-300'>
             PROYECTOS
           </a>
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+          <a href='#' className='text-white text-xs  hover:text-gray-300'>
             MINERIA
           </a>
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+          <a href='#' className='text-white text-xs  hover:text-gray-300'>
             EQUIPAMIENTO
           </a>
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+          <a href='#' className='text-white text-xs  hover:text-gray-300'>
             CONTACTO
           </a>
-          <a href='#' className='text-white text-[10px] hover:text-gray-300'>
+          <a href='#' className='text-white text-xs  hover:text-gray-300'>
             ING/ESP
           </a>
         </div>
+
         <div className='md:hidden'>
           {menuOpen && (
-            <div className='mt-2'>
-              <a href='#' className='block text-white py-2'>
+            <div className='absolute right-0 top-16 w-full bg-gray-800'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 LA EMPRESA
               </a>
-              <a href='#' className='block text-white py-2'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 PRENSA
               </a>
-              <a href='#' className='block text-white py-2'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 PROYECTOS
               </a>
-              <a href='#' className='block text-white py-2'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 MINERIA
               </a>
-              <a href='#' className='block text-white py-2'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 EQUIPAMIENTO
               </a>
-              <a href='#' className='block text-white py-2'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 CONTACTO
               </a>
-              <a href='#' className='block text-white py-2'>
+              <a
+                href='#'
+                className='text-white text-xs  hover:text-gray-300 block py-2'
+              >
                 ING/ESP
               </a>
             </div>
