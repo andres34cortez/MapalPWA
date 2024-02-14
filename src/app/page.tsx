3b62fabf253f5b4f3e65668c47cd3d7e2eb74a59
Bruno Bianchi <bianchi.b95@gmail.com>
2024-02-14 14:13:34 -0300

    content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { Content } from "@/screens/home/Content";
 import { HeroCarousel } from "@/screens/home/HeroCarousel";
 import Metricas from "@/screens/home/Metricas";
 
 export default function Home() {
   return (
-    <div className='flex flex-col w-full items-center'>
+    <div className="flex flex-col w-full items-center">
       <HeroCarousel />
       <Metricas />
+      <Content />
     </div>
   );
 }
---
+++ src/screens/home/Content.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as React from "react";
+
+import Triangulos from "@/assets/triangulos.png";
+
+export function Content() {
+  return (
+    <div
+      className={`bg-[url('/assets/triangulos.png')] h-screen w-screen`}
+    ></div>
+  );
+}
