db8efe0aa3637f600ba17f5e964c36c8487f8dfe
andres34cortez <andrescel34@gmail.com>
2024-04-27 18:25:04 -0300

    responsive home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,12 +9,12 @@ import linkdin from "@/assets/linkdin.png";
 
 export default function Footer() {
   return (
-    <div className=' w-full h-[400px] bg-black pt-24'>
-      <div className='flex flex-row gap-24 text-white items-center justify-center '>
+    <div className='w-full bg-black pt-10 sm:pt-24'>
+      <div className='flex flex-col sm:flex-row gap-4 sm:gap-24 text-white items-center justify-center px-4 sm:px-0'>
         <div>
-          <Image src={LogoFooter} alt='hola'></Image>
+          <Image src={LogoFooter} alt='Logo' />
         </div>
-        <div className=' flex flex-col gap-2'>
+        <div className='flex flex-col gap-2 sm:items-start'>
           <p className='cursor-pointer hover:underline'>LA EMPRESA</p>
           <p className='cursor-pointer hover:underline'>PRENSA</p>
           <p className='cursor-pointer hover:underline'>PROYECTO</p>
@@ -23,30 +23,28 @@ export default function Footer() {
           <p className='cursor-pointer hover:underline'>CONTACTO</p>
           <p className='cursor-pointer hover:underline'>ING/ESP</p>
         </div>
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col gap-2 sm:items-start'>
           <p>OFICINAS:</p>
           <p className='cursor-pointer hover:underline'>
             Tucumán 523 NORTE, CP 5413 Chimbas, San Juan
           </p>
-          <p className='pt-5'></p>
-          <p>CONTÁTENOS:</p>
+          <p>CONTÁCTENOS:</p>
           <p className='cursor-pointer hover:underline'>(+54) 2645 4313310</p>
           <p className='cursor-pointer hover:underline'>(+54) 2645 4313355</p>
-
           <p className='cursor-pointer hover:underline'>
             comercial@mapal.com.ar
           </p>
         </div>
         <div>Descargas</div>
         <div className='flex gap-2'>
-          <Image className='w-6 h-6' src={linkdin} alt=''></Image>
-          <Image className='w-6 h-6' src={instagram} alt=''></Image>
-          <Image className='w-6 h-6' src={Facebook} alt=''></Image>
-          <Image className='w-6 h-6' src={x} alt=''></Image>
-          <Image className='w-6 h-6' src={youtube} alt=''></Image>
+          <Image className='w-6 h-6' src={linkdin} alt='LinkedIn' />
+          <Image className='w-6 h-6' src={instagram} alt='Instagram' />
+          <Image className='w-6 h-6' src={Facebook} alt='Facebook' />
+
+          <Image className='w-6 h-6' src={youtube} alt='YouTube' />
         </div>
       </div>
-      <div className='w-[95%] h-[2px] bg-[#FDBA13] mt-10 ml-[50px]'></div>
+      <div className='w-full h-1 bg-[#FDBA13] mt-4 sm:mt-10 ml-4 sm:ml-[50px]'></div>
     </div>
   );
 }
--- src/components/HistoryBaner.tsx
+++ src/components/HistoryBaner.tsx
@@ -5,39 +5,72 @@ import Image from "next/image";
 
 export default function HistoryBaner() {
   return (
-    <div className="w-[100%] bg-black flex flex-row items-center justify-center gap-12 overflow-hidden">
-      <div className="bg-historyTexture w-[100%] h-[372px] -mt-4 absolute" />
-      <Image src={fondoHistory} alt="" className="absolute w-fullh-[372px]" />
-      <div className="border border-[#FDBA13] rounded-sm">
+    <div className='bg-black w-full'>
+      <div className='hidden md:flex w-full items-center justify-center gap-12 overflow-hidden '>
+        <div className='bg-historyTexture w-full h-[372px] -mt-4 absolute' />
         <Image
-          src={iconHistory}
-          alt=""
-          className="-mt-2 -ml-2 pb-2 pr-2 min-w-[200px]"
+          src={fondoHistory}
+          alt=''
+          className='absolute w-full h-[372px]'
         />
+        <div className='border border-[#FDBA13] rounded-sm'>
+          <Image
+            src={iconHistory}
+            alt=''
+            className='-mt-2 -ml-2 pb-2 pr-2 min-w-[200px]'
+          />
+        </div>
+        <div className='max-w-[800px]'>
+          <h1 className='text-[#FAB918] text-3xl mt-8 '>
+            NUESTRA <br /> HISTORIA
+          </h1>
+          <h2 className='text-[#FFFFFF] text-sm  font-medium pt-4'>
+            MAPAL S.A. es una empresa Sanjuanina de alcance nacional con más de
+            50 años de experiencia en construcción de Obras Viales, Civiles,
+            Hidráulicas, Servicios mineros, Servicios petroleros y desarrollo
+            inmobiliario.
+          </h2>
+          <h3 className='text-[#bababa] text-sm pt-8'>
+            Desde su comienzo en el año 1967 bajo el nombre de CONSTRUCCIONES
+            JULIO CESAR PALLUCCHINI y posteriormente a partir del año 1980,
+            MAPAL S.A. ha realizado un gran número de obras y servicios en la
+            provincia de San Juan y en distintas provincias de nuestro País,
+            posicionándola como una de las principales compañías de zona cuyo en
+            el rubro.
+          </h3>
+          <h3 className='text-[#bababa] text-sm italic pt-8 pb-8'>
+            “Desde su comienzo en el año 1967 bajo el nombre de CONSTRUCCIONES{" "}
+            <br />
+            JULIO CESAR PALLUCCHINI y posteriormente a partir del año 1980,
+            MAPAL.”
+          </h3>
+        </div>
       </div>
-      <div className="max-w-[800px]">
-        <h1 className="text-[#FAB918] text-3xl mt-8 ">
-          NUESTRA <br /> HISTORIA
-        </h1>
-        <h2 className="text-[#FFFFFF] text-sm  font-medium pt-4">
-          MAPAL S.A. es una empresa Sanjuanina de alcance nacional con más de 50
-          años de experiencia en construcción de Obras Viales, Civiles,
-          Hidráulicas, Servicios mineros,Servicios petroleros y desarrollo
-          inmobiliario.
-        </h2>
-        <h3 className="text-[#bababa] text-sm pt-8">
-          Desde su comienzo en el año 1967 bajo el nombre de CONSTRUCCIONES
-          JULIO CESAR PALLUCCHINI y posteriormente a partir del año 1980, MAPAL
-          S.A. ha realizado un gran número de obras y servicios en la provincia
-          de San Juan y en distintas provincias de nuestro País, posicionándola
-          como una de las principales compañías de zona cuyo en el rubro.
-        </h3>
-        <h3 className="text-[#bababa] text-sm italic pt-8 pb-8">
-          “Desde su comienzo en el año 1967 bajo el nombre de CONSTRUCCIONES{" "}
-          <br />
-          JULIO CESAR PALLUCCHINI y posteriormente a partir del año 1980,
-          MAPAL.”
-        </h3>
+
+      <div className='md:hidden text-center'>
+        <h1 className='text-[#FAB918] text-3xl mt-8'>NUESTRA HISTORIA</h1>
+        <Image src={iconHistory} alt='' className='w-[200px] mx-auto mt-4' />
+        <div className='text-[#FFFFFF] text-sm font-medium pt-4 px-4'>
+          <p>
+            MAPAL S.A. es una empresa Sanjuanina de alcance nacional con más de
+            50 años de experiencia en construcción de Obras Viales, Civiles,
+            Hidráulicas, Servicios mineros, Servicios petroleros y desarrollo
+            inmobiliario.
+          </p>
+          <p className='pt-4'>
+            Desde su comienzo en el año 1967 bajo el nombre de CONSTRUCCIONES
+            JULIO CESAR PALLUCCHINI y posteriormente a partir del año 1980,
+            MAPAL S.A. ha realizado un gran número de obras y servicios en la
+            provincia de San Juan y en distintas provincias de nuestro País,
+            posicionándola como una de las principales compañías de zona cuyo en
+            el rubro.
+          </p>
+          <p className='italic pt-4 pb-8'>
+            “Desde su comienzo en el año 1967 bajo el nombre de CONSTRUCCIONES
+            JULIO CESAR PALLUCCHINI y posteriormente a partir del año 1980,
+            MAPAL.”
+          </p>
+        </div>
       </div>
     </div>
   );
