bbde7cc6f7acec2e0894b191549bf41f2a33f1f5
andres34cortez <andrescel34@gmail.com>
2024-04-29 17:49:56 -0300

    iconos y fixes

---
+++ src/components/PhotosHomeBanner.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+import photo1 from "@/assets/photosBaner/photo1.png";
+import photo2 from "@/assets/photosBaner/photo2.png";
+import photo3 from "@/assets/photosBaner/photo3.png";
+import photo4 from "@/assets/photosBaner/photo4.png";
+import photo5 from "@/assets/photosBaner/photo5.png";
+import sigma from "@/assets/photosBaner/sigma.png";
+import sigmaLogo from "@/assets/photosBaner/SigmaPNG.png";
+export default function PhotosHomeBanner() {
+  return (
+    <div className='overflow-hidden'>
+      <div className='flex flex-row'>
+        <Image src={photo1} alt=''></Image>
+        <Image src={photo2} alt=''></Image>
+        <Image src={photo3} alt=''></Image>
+        <Image src={photo4} alt=''></Image>
+        <Image src={photo5} alt=''></Image>
+      </div>
+      <div className='relative'>
+        <Image src={sigma} className='relative z-10' alt=''></Image>
+        <Image
+          className='absolute top-[40%] right-[224px] z-20'
+          src={sigmaLogo}
+          alt=''
+        ></Image>
+      </div>
+    </div>
+  );
+}
--- src/screens/proyectos/Data.tsx
+++ src/screens/proyectos/Data.tsx
@@ -1,44 +1,47 @@
+import Image from "next/image";
+import Viviendas from "@/assets/houseMetrics.png";
+import Naves from "@/assets/naves.png";
+import Departamentos from "@/assets/departamentos.png";
+import Civiles from "@/assets/civiles.png";
 export default function Data() {
   return (
-    <div className="w-screen md:w-[800px] grid grid-cols-2 md:grid-cols-4 gap-[35px] mx-auto p-[30px]">
-      <div className="flex flex-col gap-2 justify-center items-center md:items-start">
-        <div className="flex gap-3">
-          <p>{":)"}</p>
-          <p>4088</p>
+    <div className='w-screen md:w-[800px] grid grid-cols-2 md:grid-cols-4 items-center gap-[35px] mx-auto p-[30px]'>
+      <div className='flex flex-col  items-start justify-start'>
+        <div className='flex items-center gap-2 justify-start mb-2 '>
+          <Image src={Viviendas} alt='' className='w-8 h-8' />
+          <h3 className='text-xl font-medium'>4.088</h3>
         </div>
-        <p className="text-sm text-center md:text-start">
-          Viviendas de Barrios Construidas
-        </p>
+        <h3 className='text-sm text-gray-600 text-left'>
+          Viviendas de Barrios Construidas.-
+        </h3>
       </div>
-
-      <div className="flex flex-col gap-2 justify-center items-center md:items-start">
-        <div className="flex gap-3">
-          <p>{":)"}</p>
-          <p>4088</p>
+      <div className='flex flex-col  items-start justify-start  mb-2 '>
+        <div className='flex items-center gap-2'>
+          <Image src={Departamentos} alt='' className='w-8 h-8' />
+          <h3 className='text-xl font-medium'>550</h3>
         </div>
-        <p className="text-sm text-center md:text-start">
-          Viviendas de Barrios Construidas
-        </p>
+        <h3 className='text-sm text-gray-600 text-left'>
+          {" "}
+          Departamentos en Edificios en Altura Realizados.-
+        </h3>
       </div>
-
-      <div className="flex flex-col gap-2 justify-center items-center md:items-start">
-        <div className="flex gap-3">
-          <p>{":)"}</p>
-          <p>4088</p>
+      <div className='flex flex-col  items-start justify-start  mb-2'>
+        <div className='flex items-center gap-2 mb-3'>
+          <Image src={Naves} alt='' className='w-8 h-8' />
+          <h3 className='text-xl font-medium'>16</h3>
         </div>
-        <p className="text-sm text-center md:text-start">
-          Viviendas de Barrios Construidas
-        </p>
+        <h3 className='text-sm text-gray-600 text-left'>
+          Naves industriales Contruidas
+        </h3>
       </div>
-
-      <div className="flex flex-col gap-2 justify-center items-center md:items-start">
-        <div className="flex gap-3">
-          <p>{":)"}</p>
-          <p>4088</p>
+      <div className='flex flex-col  items-start justify-start  mb-2'>
+        <div className='flex items-center gap-2 mb-3'>
+          <Image src={Civiles} alt='' className='w-8 h-8' />
+          <h3 className='text-xl font-medium'>285.201</h3>
         </div>
-        <p className="text-sm text-center md:text-start">
-          Viviendas de Barrios Construidas
-        </p>
+        <h3 className='text-sm text-gray-600 text-left'>
+          M2 De Construcciones Civiles Realizadas
+        </h3>
       </div>
     </div>
   );
