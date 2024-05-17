da393dd5530557aeff22f942ef3b48941e8e93c2
andres34cortez <andrescel34@gmail.com>
2024-05-17 15:43:18 -0300

    baner fotos inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,19 @@ import { Content } from "@/screens/home/Content";
 import HistoryBaner from "@/components/HistoryBaner";
 import { HeroCarousel } from "@/screens/home/HeroCarousel";
 import Metricas from "@/screens/home/Metricas";
+import PhotosHomeBanner from "@/components/PhotosHomeBanner";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full items-center overflow-x-hidden">
+    <div className='flex flex-col w-full items-center overflow-x-hidden'>
       <HeroCarousel />
       <Metricas />
       <HistoryBaner />
-      <div id="empresa" className="-z-30 h-[90px]" />
+      <div id='empresa' className='-z-30 h-[90px]' />
       <Content />
+      <div className='hidden lg:block w-full'>
+        <PhotosHomeBanner />
+      </div>
     </div>
   );
 }
